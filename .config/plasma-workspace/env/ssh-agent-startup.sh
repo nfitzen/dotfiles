8942d4d6e88476ae5d919834c191b8ec7c738f5b
Nathaniel F. <nathaniel@fitzenrider.com>
2024-01-30 14:28:48 -0500

    Use systemd for SSH agent
    also fix SSH config

--- .config/plasma-workspace/env/ssh-agent-startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2023 nfitzen <https://github.com/nfitzen>
-
-[ -z "$SSH_AGENT_PID" ] && eval `ssh-agent -s`
-
-export SSH_ASKPASS=/usr/bin/ksshaskpass
