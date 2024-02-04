51bed9291d55f643faf53b9a87e57806889e0c8f
Nathaniel F. <nathaniel@fitzenrider.com>
2024-02-04 03:19:33 -0500

    Add support for private aliases

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: (C) 2022-2023 nfitzen <https://github.com/nfitzen>
+# SPDX-FileCopyrightText: (C) 2022-2024 nfitzen <https://github.com/nfitzen>
 
 alias \
     pt4="ping -c4 1.1.1.1" \
@@ -11,3 +11,5 @@ alias pcc="gcc -g -Og -Wall -Wconversion"
 alias occ="gcc -O2 -Wall -Wconversion"
 alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
 alias gdbd="gdb -ex 'set debuginfod enabled off'"
+
+[ -f $HOME/.dotfiles/private_aliases.sh ] && source $HOME/.dotfiles/private_aliases.sh
