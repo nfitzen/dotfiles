b86c2be02d38e5c31edf00cf80c7fcc11528442b
Nathaniel Fitzenrider <nathaniel@fitzenrider.com>
2024-08-07 20:10:56 -0400

    Use shared electron flags

--- .config/plasma-workspace/env/wayland-app-enable.sh
+++ .config/plasma-workspace/env/wayland-app-enable.sh
@@ -3,5 +3,6 @@
 # SPDX-FileCopyrightText: 2023-2024 nfitzen <https://github.com/nfitzen>
 
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
-export MOZ_ENABLE_WAYLAND=1
+    export MOZ_ENABLE_WAYLAND=1
+    export ELECTRON_OZONE_PLATFORM_HINT=auto
 fi
