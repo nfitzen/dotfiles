7263eb891703123e4adb355aadd38d17f13001f9
Nathaniel F. <nathaniel@fitzenrider.com>
2024-09-05 16:10:41 -0400

    Use IPv4 for basic ping test

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -5,7 +5,7 @@
 alias \
     pt4="ping -c4 1.1.1.1" \
     pt6="ping -c4 2606:4700:4700::1111" \
-    pt=pt6
+    pt=pt4
 
 alias pcc="gcc -g -Og -Wall -Wconversion"
 alias occ="gcc -O2 -Wall -Wconversion"
